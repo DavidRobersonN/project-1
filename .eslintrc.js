ad7060562bcbc8495fea6b2f894138c3d7e3a176
David Roberson <david.roberson2010@gmail.com>
2025-03-19 00:46:17 -0300

    aparentemente corrido problemas de compatibilidade com eslint, ficou apenas com erro no textInput, preccisa verificar se nao é erro de codigo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,16 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'jest': true,
-    'node': true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
+    'plugin:jest/recommended',
     'plugin:react/recommended',
-    'prettier',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
-    'plugin:jest/recommended',
-    "plugin:react/jsx-runtime",
   ],
   globals: {
     Atomics: 'readonly',
@@ -20,20 +18,19 @@ module.exports = {
   },
   parser: '@babel/eslint-parser',
   parserOptions: {
-    requireConfigFile: false,
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier', 'react-hooks', 'jsx', 'jest'],
+  plugins: ['react', 'prettier', 'jest', 'react-hooks'],
   settings: {
     react: {
       version: 'detect',
     },
   },
   rules: {
-    'react/react-in-jsx-scope': 0,
+    'react/react-in-jsx-scope': 'off',
   },
 };
--- src/components/TextImput/TextInput.spec.jsx
+++ src/components/TextImput/TextInput.spec.jsx
@@ -8,14 +8,16 @@ import userEvent from '@testing-library/user-event';
 describe('<TextInput />', () => {
   it('should have a value of searchValue', () => {
     const fn = jest.fn();
+
     render(<TextInput handleChange={fn} searchValue={'testando'} />);
     const input = screen.getByPlaceholderText(/type your search/i);
+
     expect(input.value).toBe("testando");
   });
 
-  it('should call handle function on each key pressed', () => {
+  it('should call handleChange function on each key pressed', () => {
     const fn = jest.fn(); // criando uma função aleatoria
-    render(<TextInput handleChange={fn} />); // passando uma função aleatoria, apenas para test
+    render(<TextInput handleChange={fn} searchValue={'o valor'} />); // passando uma função aleatoria, apenas para test
 
     const input = screen.getByPlaceholderText(/type your search/i);
     const value = 'o valor';
@@ -23,6 +25,7 @@ describe('<TextInput />', () => {
     userEvent.type(input, value); // utilizando uma função da testlibrary para criar o evento de digitar no input, simulando o usuario
 
     expect(input.value).toBe(value); // verificando se o valor do input é o mesmo que digitamos
+
     expect(fn).toHaveBeenCalledTimes(value.length); // verificando se a quantidade de teclas que digitamos, foi a mesma quantidade de vezes que a função foi chamada
   });
 });
